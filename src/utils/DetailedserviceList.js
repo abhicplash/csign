892b8a5027ba621d75d2ed6a816ef5b5dce2358f
Abhilash CP <abhicplash@gmail.com>
2024-10-17 11:03:49 +0530

    id value corrected

--- src/utils/DetailedserviceList.js
+++ src/utils/DetailedserviceList.js
@@ -17,7 +17,7 @@ import image16 from "../assets/services/scanning.JPG";
 
 export const fullServiceListDetailed = [
   {
-    id: 1,
+    id: 0,
     image: image7,
     service: "Business Cards",
     head: "Make a Lasting Impression with Premium Business Cards",
@@ -25,7 +25,7 @@ export const fullServiceListDetailed = [
       "Your business card is often the first impression you make on potential clients and partners. At Creative Signature Advertisement, we understand the importance of having a professional and memorable business card. We offer a variety of custom designs, finishes, and materials, allowing you to create a business card that reflects your brand identity and personal style. Choose from options like matte, glossy, or textured finishes, and select from standard or custom sizes. Our high-quality printing ensures that your business cards are sharp, vibrant, and durable, making them a powerful networking tool. Stand out in the competitive market with our exceptional business card printing services!"
   },
   {
-    id: 2,
+    id: 1,
 
     image: image1,
     service: "Flyers",
@@ -34,7 +34,7 @@ export const fullServiceListDetailed = [
       "At Creative Signature Advertisement, we specialize in creating visually stunning flyers designed to captivate your target audience and effectively convey your message. Our team of experienced graphic designers works closely with you to develop custom flyers that highlight your events, promotions, or product launches. With various sizes, finishes, and paper quality options available, we ensure that each flyer is tailored to your specific needs and branding requirements. Using high-quality printing techniques, we guarantee vibrant colors and sharp details that make your flyers stand out. Let us help you enhance your visibility and drive engagement with our exceptional flyer printing services!"
   },
   {
-    id: 3,
+    id: 2,
 
     image: image2,
     service: "T-Shirt Printing",
@@ -43,7 +43,7 @@ export const fullServiceListDetailed = [
       "Transform your ideas into wearable art with our top-notch T-shirt printing services at Creative Signature Advertisement. Perfect for businesses, schools, events, and personal gifts, our custom T-shirt printing allows you to showcase your unique designs and logos. We offer a wide variety of T-shirt styles, colors, and sizes to choose from, ensuring that your custom apparel fits your vision perfectly. Our advanced printing technology guarantees vibrant colors that are durable and long-lasting, making your T-shirts suitable for any occasion. Whether you need a single shirt or a bulk order, we are here to bring your creative concepts to life!"
   },
   {
-    id: 4,
+    id: 3,
 
     image: image4,
     service: "Mug Printing",
@@ -52,7 +52,7 @@ export const fullServiceListDetailed = [
       "Personalized mugs are not only practical but also make great gifts and promotional items. At Creative Signature Advertisement, we provide high-quality mug printing services that allow you to customize your mugs with your designs, logos, or messages. Our mugs are perfect for corporate giveaways, events, or personal use, ensuring that your brand stays in the spotlight. With a range of colors and styles available, we use durable printing techniques that ensure vibrant colors and lasting quality. Sip your favourite beverage in style with our custom mugs that are sure to impress!"
   },
   {
-    id: 5,
+    id: 4,
 
     image: image3,
     service: "Vehicle Stickers",
@@ -61,7 +61,7 @@ export const fullServiceListDetailed = [
       " Enhance your brand visibility with our high-quality vehicle stickers at Creative Signature Advertisement. Our custom-designed vehicle stickers are perfect for businesses looking to promote their services on the go. Made from durable materials that withstand weather conditions, our stickers ensure that your branding remains vibrant and intact. Whether you want to showcase your logo, advertise a special offer, or display a personal message, our team will help you create eye-catching designs that attract attention on the road. Transform your vehicle into an effective marketing tool with our professional vehicle sticker services!"
   },
   {
-    id: 6,
+    id: 5,
 
     image: image13,
     head: "Portable Advertising Solutions with High-Quality Roll-Up Banners",
@@ -70,7 +70,7 @@ export const fullServiceListDetailed = [
       " Our roll-up banners are an essential marketing tool for trade shows, conferences, and promotional events. At Creative Signature Advertisement, we design eye-catching roll-up banners that are easy to transport and set up, allowing you to showcase your brand message effectively. With high-quality prints that grab attention and convey your information clearly, our banners are perfect for creating a strong presence at any event. Choose from various sizes and materials to find the perfect fit for your marketing needs. Elevate your advertising game with our professional roll-up banner solutions!"
   },
   {
-    id: 7,
+    id: 6,
 
     image: image5,
     service: "Laminations",
@@ -79,7 +79,7 @@ export const fullServiceListDetailed = [
       " At Creative Signature Advertisement, we offer expert lamination services designed to preserve and enhance your important documents. Whether you need to protect menus, brochures, certificates, or presentations, our lamination adds a professional finish while increasing durability. We provide a range of lamination options, including matte and glossy finishes, ensuring that your documents not only look great but also withstand wear and tear. Keep your materials safe and presentable with our reliable lamination services that cater to all your business needs!"
   },
   {
-    id: 8,
+    id: 7,
 
     image: image10,
     service: "Binding",
@@ -88,7 +88,7 @@ export const fullServiceListDetailed = [
       "Keep your documents organized and visually appealing with our binding services at Creative Signature Advertisement. We offer a variety of binding options, including spiral, comb, and thermal binding, to suit your specific project needs. Whether you are preparing reports, presentations, or portfolios, our binding solutions add a polished touch and ensure that your materials stay intact. Our attention to detail and commitment to quality guarantee that your documents will make a strong impression. Choose our binding services for an elegant finish that reflects your professionalism!"
   },
   {
-    id: 9,
+    id: 8,
 
     image: image8,
     service: "Sign Boards",
@@ -97,7 +97,7 @@ export const fullServiceListDetailed = [
       " At Creative Signature Advertisement, we specialize in creating custom sign boards that effectively communicate your brand message. Our experienced design team works with you to develop eyecatching signs tailored to your specifications, whether for indoor or outdoor use. Using high-quality materials and printing techniques, we ensure that your sign boards are not only visually appealing but also durable and weather-resistant. Increase your visibility and attract customers with our professional sign board services that help you make a lasting impact!"
   },
   {
-    id: 10,
+    id: 9,
 
     image: image9,
     service: "Certificate Printing",
@@ -106,7 +106,7 @@ export const fullServiceListDetailed = [
       "Recognize accomplishments and milestones with our premium certificate printing services at Creative Signature Advertisement. We offer customizable templates that allow you to create elegant certificates for awards, recognitions, diplomas, and more. Our high-quality printing ensures that each certificate is beautifully presented, making it a meaningful keepsake for recipients. Celebrate success with professionally printed certificates that leave a lasting impression!"
   },
   {
-    id: 11,
+    id: 10,
 
     image: image6,
     service: "Stamp Services",
@@ -115,7 +115,7 @@ export const fullServiceListDetailed = [
       " Get high-quality custom stamps made quickly and efficiently at Creative Signature Advertisement. Our stamp services are perfect for businesses and individuals alike, allowing you to create clear and precise impressions on various surfaces. Choose from a wide range of sizes and designs to meet your specific needs, whether for office use or personal projects. Our durable stamps ensure that your branding and messages are delivered with quality and consistency. Make your mark with our reliable custom stamp services!"
   },
   {
-    id: 12,
+    id: 11,
 
     image: image14,
     service: "Frosted Stickers",
@@ -124,7 +124,7 @@ export const fullServiceListDetailed = [
       " Frosted stickers are a sophisticated way to enhance your branding efforts. At Creative Signature Advertisement, we offer custom frosted stickers that are perfect for windows, packaging, and promotional materials. These stylish stickers provide a unique look that sets your brand apart, making them ideal for a range of applications. Made from durable materials, our frosted stickers are designed to last while maintaining their elegant appearance. Elevate your marketing strategy with our high-quality frosted stickers!"
   },
   {
-    id: 13,
+    id: 12,
 
     image: image11,
     service: " Memento Works",
@@ -134,7 +134,7 @@ export const fullServiceListDetailed = [
   },
 
   {
-    id: 14,
+    id: 13,
 
     image: image15,
     service: "Project Submittals and Resumes",
@@ -143,7 +143,7 @@ export const fullServiceListDetailed = [
       "At Creative Signature Advertisement, we specialize in creating visually stunning flyers designed to captivate your target audience and effectively convey your message. Our team of experienced graphic designers works closely with you to develop custom flyers that highlight your events, promotions, or product launches. With various sizes, finishes, and paper quality options available, we ensure that each flyer is tailored to your specific needs and branding requirements. Using high-quality printing techniques, we guarantee vibrant colors and sharp details that make your flyers stand out. Let us help you enhance your visibility and drive engagement with our exceptional flyer printing services!"
   },
   {
-    id: 15,
+    id: 14,
 
     image: image12,
     service: " Typing Services",
@@ -152,7 +152,7 @@ export const fullServiceListDetailed = [
       "  Need help with typing your documents? At Creative Signature Advertisement, we offer reliable typing services that are both accurate and efficient. Whether you require typing for reports, letters, or essays, our skilled team is here to ensure your documents are ready on time and formatted correctly. Trust us to handle your typing needs professionally and help you focus on your core activities!"
   },
   {
-    id: 16,
+    id: 15,
 
     image: image16,
     service: "Scanning Services",
