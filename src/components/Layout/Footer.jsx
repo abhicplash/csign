fac9f33e28820077918eab970874638a9e6f393c
Abhilash CP <abhicplash@gmail.com>
2024-11-07 16:20:19 +0530

    final submission

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -3,21 +3,44 @@ import "./Footer.css";
 import footerlogo from "../../assets/logo/footer.png";
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
-import { FaPinterest } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 import { Link } from "react-router-dom";
+import { FaLinkedin } from "react-icons/fa6";
 
 const Footer = () => {
   return (
     <div className="footer-container">
       <div className="footer-logo-main">
         <img src={footerlogo} alt="" />
-        <div className="footer-smedia">
-          <FaFacebookSquare className="icon" />
-          <FaInstagram className="icon" />
-          <FaPinterest className="icon" />
-        </div>
       </div>
       <div className="footer-logo">
+        <span className="foot-head">Company Links</span>
+        <span className="foot-list">
+          <Link to={"/"}>
+            <span>home</span>
+          </Link>
+          <Link to={"/about"}>
+            <span>About us</span>
+          </Link>{" "}
+          <Link to={"/services"}>
+            <span>services</span>
+          </Link>{" "}
+          <Link to={"/contact"}>
+            <span>contact us</span>
+          </Link>
+        </span>
+      </div>
+      <div className="footer-logo">
+        <span className="foot-head">Contact us</span>
+        <a href="">+971 547488941</a>
+        <div className="socialLinks">
+            <FaFacebookSquare  className="footer-socio-Link"/>
+            <FaInstagram  className="footer-socio-Link"/>
+            <FaLinkedin  className="footer-socio-Link"/>
+            <FaXTwitter  className="footer-socio-Link"/>
+        </div>
+      </div>
+      {/* <div className="footer-logo">
         <span className="foot-head">contact us</span>
         <span className="foot-list">
           <span>+971-542411778</span>
@@ -34,24 +57,7 @@ const Footer = () => {
           <span>Vehicle-Printing</span>
           <span>Stamp</span>
         </span>
-      </div>
-      <div className="footer-logo">
-        <span className="foot-head">Quick Links</span>
-        <span className="foot-list">
-          <Link to={"/"}>
-            <span>home</span>
-          </Link>
-          <Link to={"/about"}>
-            <span>About us</span>
-          </Link>{" "}
-          <Link to={"/services"}>
-            <span>services</span>
-          </Link>{" "}
-          <Link to={"/contact"}>
-            <span>contact us</span>
-          </Link>
-        </span>
-      </div>
+      </div> */}
     </div>
   );
 };
